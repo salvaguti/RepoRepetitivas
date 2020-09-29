993e4e9d157897eecbf26e8699e573a87d25b55c
Salva Gutiérrez <salva.guti@gmail.com>
2020-09-29 17:56:05 +0200

    Primero

---
+++ tabladelXenter.js
@@ -0,0 +1,17 @@
+// Programa que realiza la tabla de multiplicar de un número
+// suministrado en una variable.
+
+let readlineSync = require("readline-sync");
+let num = readlineSync.questionInt("Dime la tabla a realizar: ");
+
+// De forma creciente
+for (let i=1; i<=10; i++)
+    console.log(num + ' x ' + i + ' = ' + num*i);
+
+console.log('------');
+// De forma decreciente
+for (let i=10; i>=1; i--)
+    console.log(num + ' x ' + i + ' = ' + num*i);
+
+
+
